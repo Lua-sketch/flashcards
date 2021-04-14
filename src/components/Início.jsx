dcd6ca579c5633ba701c6387ce40d722918c04bb
Luanna Veroneze <luanna.v.quinalha@gmail.com>
2021-04-14 10:31:25 -0300

    testing from different computer

--- src/components/Início.jsx
+++ src/components/Início.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 function Início(){
     return(
-      <div>
+      <div style={{background_color:'white'}}>
         <p>Escolha o tópico que deseja revisar acima!</p>
       </div>
     )
